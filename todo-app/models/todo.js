b624109c410682ef65ef7a81ab4ffdeb019b13ae
SEENIVASAN_T <89895557+SEENIVASANIT@users.noreply.github.com>
2023-03-09 22:16:41 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -3,7 +3,11 @@ const { Model, Op } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-   
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
       // define association here
     }
@@ -85,5 +89,3 @@ module.exports = (sequelize, DataTypes) => {
   );
   return Todo;
 };
-
-
